d2a915e1e2fb3900b6f9fe2f3410fbc822911faf
Thanh Tùng <electricthanhtung@gmail.com>
2025-12-09 23:14:50 +0700

    Update electron

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import { FlintDebugSession } from './flint_debug_session';
-import { ProviderResult } from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     vscode.debug.registerDebugAdapterDescriptorFactory('flint-debug', {
